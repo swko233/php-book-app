932dc49a6ef30c04f6af8edbee8a8a879b723303
swko233 <swkon233@gmail.com>
2019-04-23 11:02:16 +0900

    トップページ追加

--- app/src/Controller/PagesController.php
+++ app/src/Controller/PagesController.php
@@ -5,33 +5,60 @@
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice
+ * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         1.2.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
-namespace App\Test\TestCase\Controller;
-use App\Controller\PagesController;
-use Cake\Core\App;
+namespace App\Controller;
 use Cake\Core\Configure;
-use Cake\Http\Response;
-use Cake\Http\ServerRequest;
-use Cake\TestSuite\IntegrationTestCase;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
 use Cake\View\Exception\MissingTemplateException;
 /**
- * PagesControllerTest class
+ * Static content controller
+ *
+ * This controller will render views from Template/Pages/
+ *
+ * @link https://book.cakephp.org/3.0/en/controllers/pages-controller.html
  */
-class PagesControllerTest extends IntegrationTestCase
+class PagesController extends AppController
 {
     /**
-     * testDisplay method
+     * Displays a view
      *
-     * @return void
+     * @param array ...$path Path segments.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
+     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function testDisplay()
+    public function display(...$path)
     {
-        $this->markTestIncomplete();
+        $count = count($path);
+        if (!$count) {
+            return $this->redirect('/');
+        }
+        if (in_array('..', $path, true) || in_array('.', $path, true)) {
+            throw new ForbiddenException();
+        }
+        $page = $subpage = null;
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        $this->set(compact('page', 'subpage'));
+        try {
+            $this->render(implode('/', $path));
+        } catch (MissingTemplateException $exception) {
+            if (Configure::read('debug')) {
+                throw $exception;
+            }
+            throw new NotFoundException();
+        }
     }
 }
